af8e4a3c8fa85b9ff93ef941193690cb238575f3
Jose Joaquin Tudela Peñarrubia <tjosejoaquin@gmail.com>
2018-06-01 16:37:42 +0200

    ejercicio 5-functions-medium/ej9.js

--- 6-functions-medium/ej9.js
+++ 6-functions-medium/ej9.js
@@ -1,12 +1,12 @@
 // crea la función extraerAcronimo
 // Esta función recibe un numero indeterminado de argumentos
 // y los recibiremos en la array input con el operador rest (...input)
-// Debe devolver un string con sus iniciales 
+// Debe devolver un string con sus iniciales
 // Por ejemplo:
 // extraerAcronimo('Estudiante','Del','Bootcamp','Fullstack','Geekshubs') === 'EDBFG'
 
 
-// 
+//
 // utilizar un bucle for of
 
 // Ha de pasar los test adjuntados.
@@ -14,7 +14,7 @@
 let extraerAcronimo = (...input) => {
     let acronimo = '';
     // Aquí tu código.  Desde aquí:
-   
+   acronimo = input.map(word => word[0]).reduce((lastword,currentword)=>lastword+currentword);
     // Hasta aquí.
     return acronimo;
 }
